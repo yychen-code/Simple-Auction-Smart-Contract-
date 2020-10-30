b4b7b8e5a9710cf56395598a8091c94f9c98ce69
Yi Yan Chen <69493779+yychen-code@users.noreply.github.com>
2020-10-30 20:06:11 +0800

    Update SimpleAuction.sol

--- SimpleAuction.sol
+++ SimpleAuction.sol
@@ -1,121 +1,144 @@
-pragma solidity >=0.5 <0.6.0;
+pragma solidity >=0.4.22 <0.6.0;
 
-contract SimpleAuction
-        {
-        // The Beneficiary who has made the highest bid address public beneficiary
-        address payable public beneficiary; 
-        // The Time at which the Auction end
-        uint public auctionEndTime; 
-        // Track address of the highest bidder public
-        address public highestBidder;      
-        //Track the higest bid amount 
-        uint public highestBid;             
-        //Allowed withdrawals of previous bids
-        mapping(address => uint) pendingReturns; 
-        //Set to true at the end to prevent any changes.
-        bool ended;    
-        //set the minimal bid value
-        uint public minBid;                         
-        //set the minimum increment for a new bid
-        uint public minIncrement;
-        //Add the name of bidders  
-        string public bidderName;
-        //add the name of the highest bidder
-        string public beneficiaryName;     
-        // create a enum for the state of goods 
-        enum State {AWAITING_PAYMENT,AWAITING_Delivery,COMFIRED_Derlivery}
-        State public currState;
+contract SimpleAuction {
+
+
+    address payable public beneficiary;
+
+    uint public auctionEndTime;
+
+    address public highestBidder;
+
+    uint public highestBid;
+
+
+    mapping(address => uint) pendingReturns;
+
+    bool ended;
+    
+
+    uint public minIncrement; 
+    uint public minRequiredStartBid; 
+    
+    uint private luckyNumber;
+    
+    uint public autoPayToBeneficiaryPeriod;
+    
+
+    event HighestBidIncreased(address bidder, uint amount);
+    
+
+    event AuctionEnded(address winner, uint amount);
+
+
+    constructor(
         
-         
-        // events that will be triggered on changes of the paramets pass in.
-        event HighestBidIncreased(address bidder, uint amount); 
-        event AuctionEnded(address winner, uint amount);
-        
-        
-        
-        
-// declare the public parameters in the constructor
-constructor(uint _biddingTime,address payable _beneficiary,string memory _beneficiaryName,uint _minBid,uint _minIncrement) 
-    public 
-        {
+        uint _biddingTime,
+        address payable _beneficiary,
+        uint _minIncrement, // Q2B(a)
+        uint _minRequiredStartBid, // Q2B(a)
+        uint _luckyNumber
+ 
+    ) public {
         beneficiary = _beneficiary;
-        beneficiaryName =_beneficiaryName;
-        auctionEndTime = now + (_biddingTime * 1 seconds);
-        minBid=_minBid;
-        minIncrement= _minIncrement;
-        //highestBidder=_highestBidder;
+        auctionEndTime = now + (_biddingTime * 1 seconds) ;
+        minIncrement = _minIncrement; // Q2B(a)
+        minRequiredStartBid = _minRequiredStartBid; // Q2B(a)
+        luckyNumber = _luckyNumber;
+    }
+
+    
+    function bid() public payable {
+        
+    
+        if (highestBid == 0) {
+            require(
+                msg.value >= minRequiredStartBid,
+                "Minimal required start bid is not met."
+            );
         }
         
-// The bid function() is to ensure:
-//1.bids are make before the end of auction time.
-//2.current bid is higher than previous bid 
-//3.current bid must be higher than the minimum set bid
-//4.The current bid must be at least the minimum increament over the current highest bid 
-//5. Ensure that bid have ether attached as deposite to ensure no fraud 
-function bid() public payable
-        {
-        require(now <= auctionEndTime,"Auction already ended.");
-        require(msg.value >= highestBid,"There already is a higher bid.");
-        require(msg.value >=minBid);
-        require(msg.value >= pendingReturns[highestBidder] + minIncrement);
-                
-        if (highestBid != 0) 
-            {
-            pendingReturns[highestBidder] += highestBid;
-            minBid = highestBid;    
-            }
-            
-            highestBidder = msg.sender;
-            highestBid = msg.value;
-            emit HighestBidIncreased(msg.sender, msg.value);
-            }
-// The withdraw() function is to ensure:
-//1. The ether is not refund to the highest bidder
-//2. if there is entry in the pendingReturns map for the given sender
-//3. if there is no entry in the pendinReturns then return false
-//4. if there is entry in the pendingReturns then set the value to zero and send the ether to the sender. Then return Ture.
+ 
+        require(
+            minIncrement <= msg.value - highestBid,
+            "Minimal increment for each bid is not met"
+        );
 
-function withdraw() public returns (bool) 
-        {
-        uint amount = pendingReturns[msg.sender];
+        require(
+            now <= auctionEndTime,
+            "Auction already ended."
+        );
+
+        require(
+            msg.value > highestBid,
+            "There already is a higher bid."
+        );
         
-        if (amount > 0)
-            {pendingReturns[msg.sender] = 0;
+        require(
+            !ended,
+            "Auction has ended. Lucky Winner appeared!"
+        );
+        
+        if (highestBid != 0) {
             
-        if (!msg.sender.send(amount))
-            {
-            pendingReturns[msg.sender] = amount;
-            return false;
+            pendingReturns[highestBidder] += highestBid;
+        }
+        
+    
+        
+        if (msg.value == (highestBid + luckyNumber + minIncrement)) {
+             auctionEnd();
+             
+        } 
+        
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+
+        emit HighestBidIncreased(msg.sender, msg.value);
+        
+    }
+
+
+    function withdraw() public returns (bool) {
+        uint amount = pendingReturns[msg.sender];A
+        if (amount > 0) {
+
+            pendingReturns[msg.sender] = 0;
+
+            if (!msg.sender.send(amount)) {
+ 
+                pendingReturns[msg.sender] = amount;
+                return false;
             }
         }
         return true;
-        }
+    }
+    
 
-// The aucitonEnd() function ensure: 
-//1. end the auction and send the highest bid to the beneficiary
-function auctionEnd() public 
-        {
-        require(now >= auctionEndTime, "Auction not yet ended.");
+    function auctionEnd() private {
+
+        //require(msg.sender == contract_owner, "Only the contract owner can end an auction.");
+        //require(now >= auctionEndTime, "Auction not yet ended.");
         require(!ended, "auctionEnd has already been called.");
-        ended = true;
-        emit AuctionEnded(highestBidder, highestBid);
-        beneficiary.transfer(highestBid);
-        }
-        
-modifier onlyHighestBidder(){
-        require(msg.sender == highestBidder, "Only buyer can call this method");
-        _;
-}
 
-function deposite() onlyHighestBidder external payable{
-        require(currState == State.AWAITING_PAYMENT, "Already Paid");
-        currState = State.AWAITING_Delivery;
-}
-function confirmDelivery() onlyHighestBidder external{
-        require(currState == State.AWAITING_Delivery, "Cannot confirm delivery");
-        beneficiary.transfer(address(this).balance);
-        currState = State.COMFIRED_Derlivery;
-}
+ 
+        ended = true;
+        autoPayToBeneficiaryPeriod = auctionEndTime * (2 * 1 seconds);
+        emit AuctionEnded(highestBidder, highestBid);
+        
+        //beneficiary.transfer(highestBid);
+    }
     
+    function highestBidderOK() public {
+        require(
+            ended, "The auction has not ended yet."    
+        );
+        require(
+            (msg.sender ==  highestBidder) || (now >= autoPayToBeneficiaryPeriod) ,
+            "Only the auction winner is allowed to release the funds or you have to wait until the autoPayToBeneficiaryPeriod is reached."
+        );
+        
+        
+        beneficiary.transfer(highestBid);
+    }
 }
-    
